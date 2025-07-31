151cc923d6b479bedee1828abc0dcdad8deeaee4
KhaledAyman22 <khaledayman2222@gmail.com>
2025-07-31 18:59:15 +0300

    migrations

--- TheFisher.DAL/Migrations/20250731154612_MissingEnitityExpenses.Designer.cs
+++ TheFisher.DAL/Migrations/20250731154612_MissingEnitityExpenses.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TheFisher.DAL;
 
@@ -11,9 +12,10 @@ using TheFisher.DAL;
 namespace TheFisher.DAL.Migrations
 {
     [DbContext(typeof(FisherDbContext))]
-    partial class FisherDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250731154612_MissingEnitityExpenses")]
+    partial class MissingEnitityExpenses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
